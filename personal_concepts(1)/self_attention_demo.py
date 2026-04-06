f943adc15ae729ea460a35f116b7c70056c8887e
Balkaran Singh <balkaran.singh2209@gmail.com>
2026-04-06 16:14:46 -0600

    Made some changes to  self attention demo and added nomalization file (Doesnt have any thing in it yet but will add a function for normalization so i can use it whenever in other files

--- personal_concepts(1)/self_attention_demo.py
+++ personal_concepts(1)/self_attention_demo.py
@@ -34,5 +34,35 @@ ax.set_xlabel('Embedding Dimension 1')
 ax.set_ylabel('Embedding Dimension 2')
 ax.set_zlabel('Embedding Dimension 3')
 
-plt.title('Token Embeddings in 3D Space')
-plt.show()
+plt.title('Token Embeddings in 3D Space')  # Set the title of the plot
+plt.show()   # Display the 3D scatter plot of token embeddings
+
+
+#Dot Product Attention Calculation
+
+query = input[1]  # "journey"
+
+att_score_2 = torch.empty(input.shape[0])
+
+for i, x_1 in enumerate(input):
+    att_score_2[i] = torch.dot( x_1, query ) # dot product between the query and each token embedding
+print("Attention scores for 'journey':", att_score_2)
+
+# Normalize the attention scores to get the attention weights, which helps us understand how much attention the model should pay to each token when processing "journey". 
+# The weights are calculated by dividing each attention score by the sum of all attention scores, ensuring that the weights sum up to 1.
+
+#there are different ways to normalize the attention scores.
+
+#1. division by the sum of attention scores
+att_weights = att_score_2 / torch.sum(att_score_2)
+
+
+print("Attention weights for 'journey':", att_weights)
+print ("Sum of attention weights:", torch.sum(att_weights))  # Should be 1.0    
+
+#2. softmax function
+att_weights_softmax = torch.nn.functional.softmax(att_score_2, dim=0)
+
+
+print("Attention weights for 'journey' (softmax):", att_weights_softmax)
+print("Sum of attention weights (softmax):", torch.sum(att_weights_softmax))  # Should be 1.0
